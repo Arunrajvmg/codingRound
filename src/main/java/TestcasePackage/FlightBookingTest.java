28e7d4ff85250d41811c0614460adfa0e09f74da
Arun Raj <arunrajvmg@gmail.com>
2019-06-05 05:44:02 +0300

    Debugged HotelBookingTest for fixing errors
    The following modifications are done
    1.	Removed unused imports
    2.	Utilised common methods from CommonFunctions.java library for driver initialization and to find whether object is available or not
    3.	Added “PageFactory.initElements(driver, this);” to create objects using POM factory(Which was missing earlier)
    4.	Modified Locators
    5.	Assertion added to avoid false positive results
    
    Unused imports in FlightBookingTest was also removed

--- src/main/java/TestcasePackage/FlightBookingTest.java
+++ src/main/java/TestcasePackage/FlightBookingTest.java
@@ -1,17 +1,8 @@
 package TestcasePackage;
-import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.interactions.Actions;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 import CommonFunctions.Package.CommonFunctions;
 
 public class FlightBookingTest {
--- src/main/java/TestcasePackage/HotelBookingTest.java
+++ src/main/java/TestcasePackage/HotelBookingTest.java
@@ -1,18 +1,22 @@
 package TestcasePackage;
-import com.sun.javafx.PlatformUtil;
+import CommonFunctions.Package.CommonFunctions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.PageFactory;
 
 public class HotelBookingTest {
-
-    WebDriver driver = new ChromeDriver();
-
-    @FindBy(linkText = "Hotels")
+    private WebDriver driver;
+  
+    @FindBy(xpath = "//*[@id='Home']/div/aside[1]/nav/ul[1]/li[2]/a[1]")
     private WebElement hotelLink;
+
+    @FindBy(id = "english_site_pref")
+    private WebElement EnglishLanguage;    
 
     @FindBy(id = "Tags")
     private WebElement localityTextBox;
@@ -22,33 +26,29 @@ public class HotelBookingTest {
 
     @FindBy(id = "travellersOnhome")
     private WebElement travellerSelection;
+    
+    @FindBy(xpath = "//*[@id='ui-id-1']/li[2]")
+    private  WebElement FromLocation;
 
+    @FindBy(xpath = "//*[@id='SearchForm']/section[2]/div[1]/dl/dt/label")
+    private  WebElement DateLabel;
+    
     @Test
     public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
-
+    	CommonFunctions CommonFunctionsObj = new CommonFunctions();
+    	driver = CommonFunctionsObj.driver;
         driver.get("https://www.cleartrip.com/");
+        PageFactory.initElements(driver, this);
+        // This exception is used to handle in regions where there are multiple languages
+        if (CommonFunctionsObj.isElementPresent(driver, By.id("english_site_pref")) == true) {
+        	EnglishLanguage.click();        }
         hotelLink.click();
-
         localityTextBox.sendKeys("Indiranagar, Bangalore");
-
+        FromLocation.click();
+        DateLabel.click();
         new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
         searchButton.click();
-
+        Assert.assertTrue(CommonFunctionsObj.isElementPresent(driver,By.linkText("Check availability")));
         driver.quit();
-
     }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
-
 }
